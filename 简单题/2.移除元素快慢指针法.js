8443ef6c0310ff68648c8042aaa609ed05b21361
LingXi <2841572483@qq.com>
2026-04-25 11:01:32 +0800

    简单题/2.移除元素快慢指针法.js

---
+++ 简单题/2.移除元素快慢指针法.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let left = 0
+    let right = 0
+    while(right<nums.length){
+      if(nums[right]!==val){
+        nums[left]=nums[right]
+        left++
+      }
+      right++
+    }
+    return left
+};
+
+let nums =[0,1,2,2,3,0,4,2]
+let val = 2
+removeElement(nums,val)
+console.log(nums)
