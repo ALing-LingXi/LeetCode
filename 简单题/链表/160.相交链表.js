e258d959232fd8d156d5f05151abbb3f677a89ca
LingXi <2841572483@qq.com>
2026-05-01 21:23:27 +0800

    简单题/链表/160.相交链表.js

---
+++ 简单题/链表/160.相交链表.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null
+  let pa = headA
+  let pb = headB
+  while (pa !== pb) {
+    pa = pa === null ? pa = headB : pa.next
+    pb = pb === null ? pb = headA : pb.next
+  }
+  return pa
+};
