bf26a1373c25243aa4a3686128d78e031256cfbf
LingXi <2841572483@qq.com>
2026-05-01 21:25:02 +0800

    中等题/子串/560.前缀哈希解法.js

---
+++ 中等题/子串/560.前缀哈希解法.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let count =0
+    let prevSum =0
+    const map = new Map()
+    map.set(0,1)
+
+};
